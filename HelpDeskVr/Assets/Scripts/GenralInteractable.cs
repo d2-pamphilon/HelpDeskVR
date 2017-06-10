e435f306dabefbbe36a45f7b2f64213f4d34b377
d2-pamphilon <danpamphilon@yahoo.co.uk>
2017-06-10 13:26:18 +0100

    VRTK & SteamVR, basic grabing

---
+++ HelpDeskVr/Assets/Scripts/GenralInteractable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenralInteractable : VRTK.VRTK_InteractableObject
+{
+    public override void StartUsing(GameObject usingObject)
+    {
+        base.StartUsing(usingObject);
+       
+    }
+
+    public override void StopUsing(GameObject usingObject)
+    {
+        base.StopUsing(usingObject);
+       
+    }
+
+    protected void Start()
+    {
+        
+    }
+
+    protected override void Update()
+    {
+        base.Update();     
+    }
+}
