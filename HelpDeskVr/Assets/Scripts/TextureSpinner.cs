5d0efdbd7f9d7a246ea778e49aee9ab7b96c2dcf
d2-pamphilon <danpamphilon@yahoo.co.uk>
2017-06-20 19:15:36 +0100

    Wanacry scene
    my eyes are bleeding PLEASE SEND HELP

---
+++ HelpDeskVr/Assets/Scripts/TextureSpinner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// commented bits are for scrolling backgrounds if needed
+public class TextureSpinner : MonoBehaviour {
+    public float speed = 0.0f;
+   // public Renderer rend;
+    //private Vector2 test;
+    // Use this for initialization
+    void Start()
+    {
+       // rend = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 1, 0) * speed);
+        //rend.sharedMaterial.mainTextureOffset = test + new Vector2(0.1f * speed * Time.deltaTime, 0);
+       // test = rend.material.mainTextureOffset;
+    }
+}
