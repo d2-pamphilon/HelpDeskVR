4291cb4631eecfc4a4c7530936c18d0f549b39f8
d2-pamphilon <danpamphilon@yahoo.co.uk>
2017-07-20 17:42:20 +0100

    heads scream
    WARNING MAY DRIVE YOU INSANE

---
+++ HelpDeskVr/Assets/Scripts/ScreamMng.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreamMng : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip[] Screams;
+    [SerializeField]
+    private float ScreamWaitTime;
+
+    private AudioSource AS;
+    [SerializeField]
+    private float timer;
+	
+	void Start ()
+    {
+        timer = 0f;
+        AS = GetComponent<AudioSource>();
+	}
+	
+	void Update ()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > ScreamWaitTime)
+        {
+            timer = 0;
+            AS.clip = Screams[Random.Range(0, Screams.Length)];
+            AS.Play();
+        }
+
+	}
+}
