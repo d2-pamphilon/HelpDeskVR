344bea318808360d93c5e7dafaade5a31ec87ba3
Nickstu14 <nickstu14@hotmail.co.uk>
2017-08-02 17:06:14 +0100

    Finnishing off Under The Sea scene
    Finnishing off the scene, the fish should now swim around then stop and turn seconds before the end of the time limit.

--- HelpDeskVr/Assets/Scripts/UnderDaSeaScripts/Fish/GlobalFlock.cs
+++ HelpDeskVr/Assets/Scripts/UnderDaSeaScripts/Fish/GlobalFlock.cs
@@ -7,11 +7,12 @@ public class GlobalFlock : MonoBehaviour
 
     public GameObject m_FishPrefab;
     public GameObject m_GoalPrefab;
-    public static int m_size = 4;
-    public static int m_max;
-    public static int m_min = 1;
+    public float m_size = 4;
+
 
     public float m_TimeLeft;
+    public float m_Time;
+    public float m_Timer = 1;
 
     static int m_NumFish = 30;
     public static GameObject[] m_AllFish = new GameObject[m_NumFish];
@@ -20,7 +21,8 @@ public class GlobalFlock : MonoBehaviour
 
     private DimensionTimer m_DimTimer;
 
-    [Space(5)][Header("Players Head")]
+    [Space(5)]
+    [Header("Players Head")]
     public Transform m_Player;
     private bool m_Onetime;
     private Vector3 m_Direction;
@@ -31,9 +33,9 @@ public class GlobalFlock : MonoBehaviour
     void Start()
     {
         m_DimTimer = GetComponentInParent<DimensionTimer>();
-        
+
         m_Onetime = true;
-        m_max = m_size;
+
 
         m_GoalPos = new Vector3(Random.Range(-m_size + transform.position.x, m_size + transform.position.x),
              transform.position.y, Random.Range(-m_size + transform.position.z, m_size + transform.position.z));
@@ -44,8 +46,6 @@ public class GlobalFlock : MonoBehaviour
             Vector3 m_pos = new Vector3(Random.Range(-m_size + transform.position.x, m_size + transform.position.x),
                  2 + transform.position.y, Random.Range(-m_size + transform.position.z, m_size + transform.position.z));
 
-
-
             m_AllFish[i] = (GameObject)Instantiate(m_FishPrefab, m_pos, Quaternion.identity, gameObject.transform);
             DimensionChanger.SetLayerRecursively(m_AllFish[i], gameObject.layer);
         }
@@ -54,23 +54,28 @@ public class GlobalFlock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        m_TimeLeft = m_DimTimer.getRemainingTime();
-
-        /*if (m_DimTimer.getRemainingTime() <= 50 && m_DimTimer.getRemainingTime() >= 20) //normal Boids
+        if (Random.Range(0, 10000) < 50)
         {
-            if (Random.Range(0, 10000) < 50)
-            {
-                m_GoalPos = new Vector3(Random.Range(-m_size + transform.position.x, m_size + transform.position.x),
-                  transform.position.y, Random.Range(-m_size + transform.position.z, m_size + transform.position.z));
+            m_GoalPos = new Vector3(Random.Range(-m_size + transform.position.x, m_size + transform.position.x),
+              transform.position.y, Random.Range(-m_size + transform.position.z, m_size + transform.position.z));
 
-                m_GoalPrefab.transform.position = m_GoalPos;
+            m_GoalPrefab.transform.position = m_GoalPos;
+        }
+
+        if (m_DimTimer.getRemainingTime() <= 20 && m_DimTimer.getRemainingTime() >= 10)
+        { //decrease the width, closer to the player
+            m_Time += Time.deltaTime;
+
+            if (m_Time >= m_Timer)
+            {
+                if (m_size >= 1.0f)
+                {
+                    m_size -= 0.01f;
+                    m_Time = 0;
+                }
             }
         }
-        else if (m_DimTimer.getRemainingTime() <= 20 && m_DimTimer.getRemainingTime() >= 10)
-        { //decrease the width, closer to the player
-
-        }
-        else*/ if (m_DimTimer.getRemainingTime() <= 115 && m_DimTimer.getRemainingTime() >= 0)
+        else if (m_DimTimer.getRemainingTime() <= 10 && m_DimTimer.getRemainingTime() >= 0)
         {//stop fish and make them look at player
             if (m_Onetime)
             {
@@ -82,19 +87,17 @@ public class GlobalFlock : MonoBehaviour
             }
             for (int i = 0; i < m_NumFish; i++)
             {
-                //m_Direction = (m_Player.position - m_AllFish[i].transform.position).normalized;
-                m_LookRotation = Quaternion.LookRotation(m_Player.position);
-                print(m_LookRotation);
-                print( m_Player.position);
+                m_Direction = (m_Player.position - m_AllFish[i].transform.position).normalized;
+                m_LookRotation = Quaternion.LookRotation(m_Direction);
                 m_AllFish[i].transform.rotation = Quaternion.Lerp(m_AllFish[i].transform.rotation, m_LookRotation, Time.deltaTime * m_RotationSpeed);
             }
         }
 
 
 
-       
 
-        
+
+
     }
 
     public Vector3 GetGoalPos()
