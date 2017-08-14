20b878cde1873f2274fc11230d22d31b6318d17c
nickstu14 <nickstu14@hotmail.co.uk>
2017-08-14 20:56:03 +0100

    Pushing Layout, script, prefabs and materials
    Started the new layout of the computers.
    Created a few computer prefabs for the room with some materials.
    added in a script that will randomly choose a prefab comp to place in a row.

---
+++ HelpDeskVr/Assets/Scripts/PublicLogin/PcRandRow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PcRandRow : MonoBehaviour
+{
+    public List<GameObject> m_Prefab;
+    public List<Transform> m_CompLoc;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        m_CompLoc = new List<Transform>();
+        int num=0;
+
+        foreach (Transform child in transform)
+        {
+            m_CompLoc.Add(child);
+            switch (Random.Range(0, 2))
+            {
+                case 0:
+                    Instantiate(m_Prefab[0], child.position, Quaternion.identity, child);
+                    break;
+                case 1:
+                    Instantiate(m_Prefab[1], child.position, Quaternion.identity, child);
+                    break;
+            }
+            num++;
+            if (num >=6)
+            {
+                return;
+            }
+        }
+
+    }
+
+}
