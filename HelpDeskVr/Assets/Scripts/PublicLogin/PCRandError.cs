0398bc0e227d16daa5eaa1c082af6d01fbf8c4ad
Nickstu14 <nickstu14@hotmail.co.uk>
2017-09-05 19:49:24 +0100

    Adding in the new Error method
    Taken out the movie clip 'code' and decided on plain text displayed on the canvas.

--- HelpDeskVr/Assets/Scripts/PublicLogin/PCRandError.cs
+++ HelpDeskVr/Assets/Scripts/PublicLogin/PCRandError.cs
@@ -5,14 +5,16 @@ using UnityEngine.UI;
 
 public class PCRandError : MonoBehaviour
 {
-    //public List<Texture> m_Errors;
-    public List<MovieTexture> m_MovieTexture;
-    public Sprite m_DeathError;
 
-    [Header("Canvas")][Space(5)]
+    [Header("Errors")]
+    [Space(5)]
+    public List<string> m_StringError;
+    public Sprite m_DeathError;
+    private int m_RandError;
+
+    [Header("Canvas")]
+    [Space(5)]
     public Canvas[] m_Canvas;
-    public Image m_Image;
-    public RawImage m_RawImage;
     private int m_CanvasNumber;
 
 
@@ -20,37 +22,24 @@ public class PCRandError : MonoBehaviour
     [Space(5)]
     public DimensionTimer m_Time;
 
-    private int m_RandError;
+
 
     // Use this for initialization
     void Start()
     {
-        // m_Errors = new List<Texture>();
-        // m_MovieTexture = new List<MovieTexture> ();
-        // m_Canvas = new List<Canvas>();
-
-        int m_rand = Random.Range(0, 100);
-        m_RandError = Random.Range(0, m_MovieTexture.Capacity);
-        m_CanvasNumber = Random.Range(0, m_Canvas.Length);
-       // m_Canvas = GetComponentsInChildren<Canvas>(); 
-
+        //get the dimentional timer
         m_Time = FindObjectOfType<DimensionTimer>();
 
-        m_Image = m_Canvas[m_CanvasNumber].GetComponentInChildren<Image>();
-        m_RawImage = m_Canvas[m_CanvasNumber].GetComponentInChildren<RawImage>();
+
+        int m_rand = Random.Range(0, 100);
+        m_RandError = Random.Range(0, m_StringError.Capacity);
+        m_CanvasNumber = Random.Range(0, m_Canvas.Length);
+
 
 
         if (m_rand <= 30)
         {
             //choose a random error to display
-
-            //m_Image[m_CanvasNumber].GetComponentInChildren<Image>().sprite = m_Errors[m_RandError];
-            m_Image.enabled = false;    //turn off the image
-            m_RawImage.enabled = true;
-            m_RawImage.color = Color.white;
-            m_RawImage.texture = m_MovieTexture[m_RandError] as MovieTexture;
-            m_MovieTexture[m_RandError].Play();
-
 
         }
         else
@@ -59,21 +48,11 @@ public class PCRandError : MonoBehaviour
             this.enabled = false;
         }
 
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (m_Time.getRemainingTime() <= 10.0f)
-        {
-            m_MovieTexture[m_RandError].Stop();
-            m_Image.enabled = true;    
-            m_RawImage.enabled = false;
-            m_Image.sprite = m_DeathError;
-            m_Image.color = Color.white;
-            this.enabled = false;
-        }
+
     }
 }
