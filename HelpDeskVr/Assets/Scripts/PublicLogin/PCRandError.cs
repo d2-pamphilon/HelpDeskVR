6efc25738ce1a8418e7d632f895b191d63bbd6fd
nickstu14 <nickstu14@hotmail.co.uk>
2017-09-07 21:56:55 +0100

    Cleaned up the placeholder and added the code
    Each login now has a "Hack Bar" loading, when at 100% the works HACKED will appear.

--- HelpDeskVr/Assets/Scripts/PublicLogin/PCRandError.cs
+++ HelpDeskVr/Assets/Scripts/PublicLogin/PCRandError.cs
@@ -11,6 +11,7 @@ public class PCRandError : MonoBehaviour
     public List<string> m_StringError;
     public Sprite m_DeathError;
     private int m_RandError;
+    public float m_IncVal;
 
     [Header("Canvas")]
     [Space(5)]
@@ -23,30 +24,35 @@ public class PCRandError : MonoBehaviour
 
     [Header("Dimensional Timer")]
     [Space(5)]
-    public DimensionTimer m_Time;
+    public DimensionTimer m_DTime;
 
+    //Time
+    private float m_Time;
 
 
     // Use this for initialization
     void Start()
     {
         //get the dimensional timer
-        m_Time = FindObjectOfType<DimensionTimer>();
+        m_DTime = FindObjectOfType<DimensionTimer>();
 
 
         int m_rand = Random.Range(0, 100);
         m_RandError = Random.Range(0, m_StringError.Capacity);
         m_CanvasNumber = Random.Range(0, m_Canvas.Length);
+        m_IncVal = Random.Range(0f, 0.05f);
 
         m_Text = m_Canvas[m_CanvasNumber].GetComponentInChildren<Text>();
         Image[] m_im = m_Canvas[m_CanvasNumber].GetComponentsInChildren<Image>();
         m_DeathImage = m_im[0];
         m_BarImage = m_im[1] ;
 
-        if (m_rand <= 30)
+        if (m_rand <= 50)
         {
             //choose a random error to display
             m_DeathImage.color = Color.white;
+            m_BarImage.enabled = true;
+            m_BarImage.fillAmount = 0;
             m_Text.enabled = true;
             m_Text.fontStyle = FontStyle.Bold;
             m_Text.text = m_StringError[m_RandError];
@@ -63,12 +69,25 @@ public class PCRandError : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (m_Time.getRemainingTime() <= 20)
+        m_Time += Time.deltaTime;
+
+        if (m_DTime.getRemainingTime() <= 20)
         {
             m_DeathImage.sprite = m_DeathError;
             m_Text.enabled = false;
             this.enabled = false;
         }
+
+        if((m_BarImage.fillAmount <= 1f)&& m_Time >= 0.25)
+        {
+            m_Time = 0;
+            m_BarImage.fillAmount += m_IncVal;
+        }
+        if (m_BarImage.fillAmount >= 1f)
+        {
+            m_BarImage.enabled = false;
+            m_Text.text = "HACKED!!";
+                }
 
         //
     }
