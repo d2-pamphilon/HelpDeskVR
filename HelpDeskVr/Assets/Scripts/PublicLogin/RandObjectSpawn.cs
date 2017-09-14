079dec36a5d0f78e84b6a702dbc7efd52f3fe00a
nickstu14 <nickstu14@hotmail.co.uk>
2017-09-14 19:45:38 +0100

    Adding more objects to make the scene look more like a cafe

---
+++ HelpDeskVr/Assets/Scripts/PublicLogin/RandObjectSpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandObjectSpawn : MonoBehaviour
+{
+    public GameObject[] m_Objects;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        int m_num = Random.Range(0, 10);
+        if (m_num == 0 || m_num == 1)
+        {
+            GameObject m_object = Instantiate(m_Objects[m_num], gameObject.transform.position, Quaternion.identity, transform);
+            DimensionChanger.SetLayerRecursively(m_object, gameObject.layer);
+        }
+    }
+
+
+}
