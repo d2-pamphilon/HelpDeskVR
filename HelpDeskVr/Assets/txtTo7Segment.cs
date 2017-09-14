210dfeb64047e1b4cc688c2c2e27f6853af61ae9
d2-pamphilon <danpamphilon@yahoo.co.uk>
2017-09-14 11:27:07 +0100

    fix-a-name-a-tron

---
+++ HelpDeskVr/Assets/txtTo7Segment.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using System;
+using UnityEngine.UI;
+
+
+public class txtTo7Segment : MonoBehaviour {
+    public string Name;
+    public GameObject[] segs;
+
+    private int counter;
+    [SerializeField]
+    VRTK_Wheel[] wheels;
+
+    public static char[] Aplhabet = {' ', 'A', 'B', 'C', 'D', 'E', 'F',
+                        'G', 'H', 'I', 'J', 'K', 'L',
+                        'M', 'N', 'O', 'P', 'Q', 'R',
+                        'S', 'T', 'U', 'V', 'W', 'X',
+                        'Y', 'Z', '1', '2', '3', '4',
+                        '5', '6', '7', '8', '9', '0'};
+
+    // Use this for initialization
+    void Start()
+    {
+        counter = 0;
+        //wheels = GetComponentsInChildren<VRTK_Wheel>();
+        //Array.Sort(wheels, delegate (VRTK_Wheel wheel1, VRTK_Wheel wheel2) {
+        //    if (wheel2.transform.position.x > wheel1.transform.position.x)
+        //    {
+        //        return 1;
+        //    }
+        //    else
+        //    {
+        //        return -1;
+        //    };
+        //});
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string str = "";
+        for (int i = 0; i < wheels.Length; i++)
+        {
+            str += Aplhabet[(int)(wheels[i].GetValue())];
+        }
+        Name = str;
+        char[] characters = Name.ToCharArray();
+        counter = 0;
+        foreach (char C in characters)
+        {
+            segs[counter].GetComponent<Display7Seg>().setChar(C);
+            counter++;
+        }
+    }
+}
