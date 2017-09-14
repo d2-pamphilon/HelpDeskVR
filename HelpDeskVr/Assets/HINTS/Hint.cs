a14db34c327e206c734609672d0da1b31f963779
gualtyphone <gualtyphone@me.com>
2017-09-14 11:34:39 +0100

    Working on Hints

---
+++ HelpDeskVr/Assets/HINTS/Hint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hint : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject attachedTo;
+
+	// Use this for initialization
+	void Start () {
+        player = GameManager.Instance.mainCamera.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(player.transform.position);
+        Vector3 NewPos = (attachedTo.transform.position + 
+                              ((player.transform.position - attachedTo.transform.position)/10));
+        NewPos.y += 0.3f;
+
+        transform.position = NewPos;
+        
+	}
+}
